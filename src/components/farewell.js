1b03f3c96b58caf638dec83cb24963248e39f103
Adetth <raju.adetth@gmail.com>
2024-05-15 20:41:03 +0530

    Date Changed

--- src/components/farewell.js
+++ src/components/farewell.js
@@ -69,13 +69,13 @@ const redirectToForm = () => {
         </header>
         <div className="container">
             <div className="text1" id="text1">
-                <h1 className="h1class"><a href=""><img src="https://fontmeme.com/permalink/240513/5391d05528dd4a312e23a0ddf02fddef.png" alt="the-last-jedi-font" border="0" className="imgClass"/></a><br/><a href=""><img src="https://fontmeme.com/permalink/240513/f1f8b9f34f00bab1f55fc0080fba1571.png" alt="the-last-jedi-font" border="0"/></a><br/><a href="https://fontmeme.com/futuristic-fonts/"><img src="https://fontmeme.com/permalink/240513/987f62bfe5e94cfcb5126a9d63021fd2.png" alt="futuristic-fonts" border="0"/></a></h1>
+                <h1 className="h1class"><a href=""><img src="https://fontmeme.com/permalink/240513/5391d05528dd4a312e23a0ddf02fddef.png" alt="the-last-jedi-font" border="0" className="imgClass"/></a><br/><a href=""><img src="https://fontmeme.com/permalink/240513/f1f8b9f34f00bab1f55fc0080fba1571.png" alt="the-last-jedi-font" border="0"/></a><br/><a href=""><img src="https://fontmeme.com/permalink/240515/ac62b1dabf96685d85795456601aaba8.png" alt="beat-the-💩-font" border="0" className='clickMe'/></a></h1>
             </div>
             <div className="text2" id="text2">
                 <h1 className="h1class"><a href=""><img src="https://fontmeme.com/permalink/240513/1ceeda1a5cef9c73d179db2332974331.png" alt="the-last-jedi-font" border="0" className="imgClass"/></a><br/><a href=""><img src="https://fontmeme.com/permalink/240513/809723eac324e43a15c4404da1098b89.png" alt="the-last-jedi-font" border="0" className="imgClass"/></a></h1>
             </div>
             <div className="text3" id="text3">
-                <h1 className="h1class"><a href=""><img src="https://fontmeme.com/permalink/240513/97fe321e5cd987c834a9920bb7112faa.png" alt="the-last-jedi-font" border="0" className="imgClass"/></a><br/><a href=""><img src="https://fontmeme.com/permalink/240513/6e57a83efff3074adf917b63884ecca0.png" alt="the-last-jedi-font" border="0" className="imgClass"/></a></h1>
+                <h1 className="h1class"><a href=""><img src="https://fontmeme.com/permalink/240513/97fe321e5cd987c834a9920bb7112faa.png" alt="the-last-jedi-font" border="0" className="imgClass"/></a><br/><a href=""><img src="https://fontmeme.com/permalink/240515/1138e1208ec624c90c59acaa4b57f3b4.png" alt="the-last-jedi-font" border="0" className='imgClass'/></a></h1>
             </div>
             <div className="text4" id="text4">
                 <h1 className="h1class"><a href=""><img src="https://fontmeme.com/permalink/240513/61d6198c7c4bba5481ffb6a0853026fe.png" alt="the-last-jedi-font" border="0" className="imgClass"/></a><br/><a href=""><img src="https://fontmeme.com/permalink/240513/c49468a39e462e67c5cee5449c7356b3.png" alt="the-last-jedi-font" border="0" className="imgClass"/></a><br/><a href=""><img src="https://fontmeme.com/permalink/240513/5fc3a0b395b8b7b711252066dea1658d.png" alt="the-last-jedi-font" border="0" className="imgClass"/></a></h1>
